ed7d4d195bf0d6209af79f6bef3020a8f57585da
Manu <dev@mfy.es>
2021-09-21 00:31:09 +0200

    ERC721 tests

--- test/royalties.test.js
+++ test/royalties.test.js
@@ -1,12 +1,31 @@
-const MinimalERC721 = artifacts.require('AssetERC721')
+const AssetERC721 = artifacts.require('AssetERC721')
 
 contract ("ERC721 with Royalties", accounts => {
 
     const [deployerAddress, tokenAddr1] = accounts;
 
     it ("is possible to mint tokens", async () => {
-        let token = await MinimalERC721.deployed();
+        let token = await AssetERC721.deployed();
         await token.mint(tokenAddr1);
-    })
+    });
+
+    it ("is possible to set royalties", async () => {
+        let token = await AssetERC721.deployed();
+        await token.setRoyalties(0, deployerAddress, 1000);
+        let royalties = await token.getRaribleV2Royalties(0);
+        assert.equal(royalties[0].value,'1000');
+        assert.equal(royalties[0].account,deployerAddress);
+    });
+
+    it ("works with ERC2981 Royalties", async () => {
+        let token = await AssetERC721.deployed();
+        let royalties = await token.royaltyInfo(0,100000);
+        assert.equal(royalties.royaltyAmount.toString(),'10000');
+        assert.equal(royalties.receiver, deployerAddress);
+    });
+
+    
+
+   
 
 })
